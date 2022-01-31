d90c61543e04aea5c27990cb430879e33c861543
Tadhg O'Rourke <orourketadhg@gmail.com>
2022-01-31 18:22:22 +0000

    panel test from GameController

--- src/ie/ucd/tor/engine/core/systems/GameController.java
+++ src/ie/ucd/tor/engine/core/systems/GameController.java
@@ -1,5 +1,11 @@
 package ie.ucd.tor.engine.core.systems;
 
+import ie.ucd.tor.engine.core.gameobject.GameObject;
+import ie.ucd.tor.engine.core.gameobject.components.Animation;
+import ie.ucd.tor.engine.core.gameobject.components.Collision;
+import ie.ucd.tor.engine.core.gameobject.components.Sprite;
+import ie.ucd.tor.engine.core.gameobject.components.data.SpriteSheetData;
+import ie.ucd.tor.engine.maths.Point2D;
 import ie.ucd.tor.engine.rendering.GameWindow;
 
 import static ie.ucd.tor.engine.rendering.GameWindow.TARGET_FPS;
@@ -20,7 +26,7 @@ public class GameController {
 	}
 
 	protected void initialization() {
-
+		panelTest();
 	}
 
 	public void gameLoop() {
@@ -46,6 +52,36 @@ public class GameController {
 			GameWindow.checkFrameRate(System.currentTimeMillis(), frameCheck);
 
 		}
+	}
+
+	private void panelTest() {
+		GameObject backgroundTest = new GameObject();
+		backgroundTest.addComponent(new Sprite("res/BackgroundPlaceholder.png", 1000, 1000, 0));
+
+		GameObject uiTest = new GameObject();
+		uiTest.addComponent(new Sprite("res/UIPlaceholder.png", 1000, 1000, 0));
+
+		GameObject spriteTestA = new GameObject();
+		spriteTestA.getTransform().setPosition(new Point2D(500 , 500));
+		spriteTestA.addComponent(new Sprite("res/TexturePlaceholder.png", 32, 32, 0));
+		spriteTestA.addComponent(new Collision(32, 32, Point2D.Zero));
+
+		GameObject spriteTestB = new GameObject();
+		spriteTestB.getTransform().setPosition(new Point2D(532 , 500));
+		spriteTestB.addComponent(new Sprite("res/TexturePlaceholder.png", 32, 32, 1));
+		spriteTestB.addComponent(new Collision(32, 32, Point2D.Zero));
+
+		GameObject animationTestA = new GameObject();
+		animationTestA.getTransform().setPosition(new Point2D(500 , 600));
+		animationTestA.addComponent(new Animation());
+		animationTestA.getComponent(Animation.class).AddAnimation("Test", new SpriteSheetData("res/AnimationPlaceholder.png", 4, 1, 4, 32, 32));
+
+		gameWindow.getBackgroundRenderer().addElement(backgroundTest);
+		gameWindow.getUiRenderer().addElement(uiTest);
+		gameWindow.getSpriteRenderer().addElement(spriteTestA);
+		gameWindow.getSpriteRenderer().addElement(spriteTestB);
+		gameWindow.getSpriteRenderer().addElement(animationTestA);
+
 	}
 
 	private void updateCollisions() {
